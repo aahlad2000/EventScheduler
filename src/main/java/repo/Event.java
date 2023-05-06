923939d8bd41ab8d3b52b910dd9f664c6dcf707f
Aahlad <aahladkethineeedi@gmail.com>
2023-05-06 14:22:28 +0530

    Initial commit

---
+++ src/main/java/repo/Event.java
@@ -0,0 +1,26 @@
+package repo;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Table
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+    private Long eventId;
+
+    @Column
+    private String eventName;
+
+    @Column
+    private String eventStatus;
+
+    @Column
+    private Date createdOn;
+
+    @Column
+    private Date scheduledTime;
+}
---
+++ src/main/java/repo/EventHistory.java
@@ -0,0 +1,35 @@
+package repo;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Table
+public class EventHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+    private Long recordId;
+
+    @Column
+    private Long eventId;
+
+    @Column
+    private String eventName;
+
+    @Column
+    private String eventStatus;
+
+    @Column
+    private Date createdOn;
+
+    @Column
+    private Date scheduledTime;
+
+    @Column
+    private Date recordedOn;
+
+    @Column
+    private String message;
+}
